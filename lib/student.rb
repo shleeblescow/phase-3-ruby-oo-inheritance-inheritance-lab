8d133e28c3ca6a8a48bc388a613ea1e58abbb0fe
Shelby Lee <slee337@uw.edu>
2022-09-21 16:48:58 -0600

    commit tree 9d209315ccccf775440810faf062f731d8c37e03

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
+class Student < User
+
+    attr_accessor :knowledge
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(k_str)
+        self.knowledge << k_str
+    end
 
 end
